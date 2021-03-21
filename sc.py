50fd8c32ec0c7a358411db6c7e08fe12be38ef71
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2021-03-21 10:13:32 -0600

    Created by bot

---
+++ sc.py
@@ -0,0 +1,26 @@
+import sys
+import time
+import pyautogui
+
+fc = open(sys.argv[1]).read().replace('\n','')
+
+exec(f't = {fc}')
+
+for dict in t:
+    if dict.get('method') == 'say':
+        for s in dict.get('text'):
+            print(s)
+    if dict.get('method') == 'create_files':
+        for s in dict.get('files'):
+            open(s, 'w')
+    if dict.get('method') == 'write_file':
+        info = dict.get('data')
+        open(info[0], 'w').write(info[1])
+    if dict.get('method') == 'wait_seconds':
+        time.sleep(int(dict.get('len')))
+    if dict.get('method') == 'move_mouse':
+        pyautogui.moveTo(dict.get('pos')[0], dict.get('pos')[1])
+    if dict.get('method') == 'comment':
+        exec(f'#{dict.get("data")}')
+    
+        
